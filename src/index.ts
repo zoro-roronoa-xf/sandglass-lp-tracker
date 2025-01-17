d6a516abf87575e8e86e98c7aa6d0925a96b6997
zoro-roronoa-xf <85159597+zoro-roronoa-xf@users.noreply.github.com>
2025-01-18 01:06:24 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { getUserToken } from "./sandglassUtls"
 
 async function main() {
   const connection = new Connection("https://mainnetbeta-rpc.eclipse.xyz", "processed");
-  const walletAddress = new PublicKey("---user wallet address");
+  const walletAddress = new PublicKey("---user wallet address---");
 
   const result = await getUserToken(connection,walletAddress)
   console.log(result)
